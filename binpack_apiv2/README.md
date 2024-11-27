340bf0aa3cff7cea66892391a1071425e8268903
Mashudu Nepfumbada <30290770+mnepfumb@users.noreply.github.com>
2024-11-27 11:56:58 +0200

    Delete binpack_apiv2 directory

--- binpack_apiv2/README.md
+++
@@ -1,3 +0,0 @@
-# binpack api
-
-npm start
--- binpack_apiv2/utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports = (errFn) => {
-    return (req, res, next) => {
-        // errFn(req, res, next).catch((err) => next(err));
-        errFn(req, res, next).catch(next);
-    };
-};
